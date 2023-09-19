3733ca87c8e487e788bfc6604760ede069263b99
~nz <81528500+ProtoSparky@users.noreply.github.com>
2023-09-19 17:06:54 +0200

    movement now working
    done:
    added player control
    added diagnoal movement
    
    todo:
    hitboxes
    enemy
    enemy spawning

---
+++ IT2/bits/19.09.2023_custom_game/README.md
@@ -0,0 +1,9 @@
+done:
+added player control
+added diagnoal movement
+
+
+todo: 
+hitboxes
+enemy
+enemy spawning
--- IT2/bits/19.09.2023_custom_game/assets/js/player_ctrl.js
+++ IT2/bits/19.09.2023_custom_game/assets/js/player_ctrl.js
@@ -1,6 +1,7 @@
 var player;
 var current_player_X;
 var current_player_Y
+var player_speed = 5;
 
 function init(){
     console.log("init start");
@@ -17,29 +18,80 @@ function init(){
    
     
 }
-
-
-document.addEventListener('keydown', function(event) {
+/*
+document.addEventListener("keydown", function(key_press){
     current_player_X = parseInt(player.style.left);
     current_player_Y = parseInt(player.style.top);
+    if(key_press.key == "w"){
+        console.log('w');
+    }
+    else if(key_press.key == "a"){
+        console.log('a');
+    }
+    else if(key_press.key == "s"){
+        console.log('s');
+    }
+    else if(key_press.key == "d"){
+        console.log('d');
+    }
+});
+*/
 
-    if (event.keyCode == 87) {
-        console.log("w was pressed"); 
-        
-        player.style.top = current_player_Y - 10; 
+//player input
 
-    }
-    else if (event.keyCode == 83) {
-        console.log("s was pressed"); 
-        player.style.top = current_player_Y + 10; 
-    }
-    else if(event.keyCode == 65){
-        console.log("a was pressed"); 
-        player.style.left = current_player_X - 10; 
-    }
-    else if(event.keyCode == 68){
-        console.log("d was pressed"); 
-        player.style.left = current_player_X + 10; 
-    }
-}, true);
+const keys = {};
+document.addEventListener("keydown",(e) =>{
+    keys[e.key] = true; //set key to active when pressed
+});
+document.addEventListener("keyup",(e)=>{
+    keys[e.key] = false;
+});
+function check_player_input(){
+    const key_W = keys["W"] || keys["w"];
+    const key_A = keys["A"] || keys["a"];
+    const key_S = keys["S"] || keys["s"];
+    const key_D = keys["D"] || keys["d"];
+    current_player_X = parseInt(player.style.left);
+    current_player_Y = parseInt(player.style.top);
+    
 
+    if(key_W && key_A){
+        console.log("wa");
+        player.style.top = (current_player_Y - player_speed);
+        player.style.left = (current_player_X - player_speed);
+    }
+    else if(key_W && key_D){
+        console.log("wd");
+        player.style.top = (current_player_Y - player_speed);
+        player.style.left = (current_player_X + player_speed);
+    }
+    else if(key_A && key_S){
+        console.log("as");
+        player.style.left = (current_player_X - player_speed);
+        player.style.top = (current_player_Y + player_speed);
+    }
+    else if(key_S && key_D){
+        console.log("sd");
+        player.style.top = (current_player_Y + player_speed);
+        player.style.left = (current_player_X + player_speed);
+    }
+    else if(key_W){
+        console.log("w");
+        player.style.top = (current_player_Y - player_speed);
+    }
+    else if(key_A){
+        console.log("a");
+        player.style.left = (current_player_X - player_speed);
+    }
+    else if(key_S){
+        console.log("s");
+        player.style.top = (current_player_Y + player_speed);
+    }
+    else if(key_D){
+        console.log("D");
+        player.style.left = (current_player_X + player_speed);
+    }
+    //console.log(key_W + "+" + key_A + "+" + key_S + "+" + key_D);
+}
+
+setInterval(check_player_input,10);
