a12faa37f57effc5b56579c22a80d04ea4ee3f3e
~nz <81528500+ProtoSparky@users.noreply.github.com>
2023-10-22 13:03:34 +0200

    working on buttons and edit feature

--- IT2/bits/16.10.2023_Visma_but_bad/assets/js/init.js
+++ IT2/bits/16.10.2023_Visma_but_bad/assets/js/init.js
@@ -1,9 +1,7 @@
 function init(){
     const TableLength = DataOP(1, true, "TableData").length;
-    console.log(DataOP(1, true, "TableData"));
     for(let CurrentTablePointer = 0; CurrentTablePointer <TableLength; CurrentTablePointer++){
         SpawnTable(DataOP(1, true, "TableData")[CurrentTablePointer],["TableArea","table0"+ CurrentTablePointer,"table",]);
-        //console.log(DataOP(1, true, "TableData")[CurrentTablePointer],["TableArea","table0"+ CurrentTablePointer,"table",]);
     }   
    
     
