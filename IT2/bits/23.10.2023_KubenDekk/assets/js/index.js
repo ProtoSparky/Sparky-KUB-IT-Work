fcdf36f285cee62eccdf45ab65e6f3984db096f1
~nz <81528500+ProtoSparky@users.noreply.github.com>
2023-10-23 16:00:13 +0200

    tire update

--- IT2/bits/23.10.2023_KubenDekk/assets/js/index.js
+++ IT2/bits/23.10.2023_KubenDekk/assets/js/index.js
@@ -1,3 +1,22 @@
 function init(){
     console.log("Inut run"); 
 }
+
+
+var ButtonTable = [
+    //Tire 1
+    [
+        '0', //Tire id 
+        'Nokian Snowproof 1',
+        'Nokian Tyres',
+        '16"', 
+        '205', 
+        '2' //0 = rim | 1 = Summer tire | 2 = winter (w/o spikes) | 
+        // 3 = winter (w spikes) | 4 = Universal
+    
+    ],
+    [
+
+    ]
+
+];
