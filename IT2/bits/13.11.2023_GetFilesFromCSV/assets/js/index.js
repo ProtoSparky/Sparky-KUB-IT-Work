120084b781e913ded9d5aa8bbb038e4f20db44cd
~nz <81528500+ProtoSparky@users.noreply.github.com>
2023-11-14 10:28:22 +0100

    woohoo 100th commit!

--- IT2/bits/13.11.2023_GetFilesFromCSV/assets/js/index.js
+++ IT2/bits/13.11.2023_GetFilesFromCSV/assets/js/index.js
@@ -66,13 +66,92 @@ function init(){
 
 
 
-    
+
 
 
     console.log(Data);
     console.log(JSON.stringify(ProcessedData));
 
-
-
+    SpawnBarChart(ProcessedData)
     
+}
+
+
+function SpawnBarChart(ProcessedData){
+    const DataLength = Object.keys(ProcessedData).length;
+    const YearKeys = Object.keys(ProcessedData);
+    /*
+    const GraphArea = document.getElementById("graph_text1");
+    const GraphWhole = document.getElementById("graph1");
+    const GraphAreaWidth = GraphArea.clientWidth;
+    const GraphAreaHeight = GraphWhole.clientHeight - GraphArea.clientHeight;
+    */
+    for(let CurrentKey = 0; CurrentKey < DataLength; CurrentKey ++){
+        let CurrentYearKey = YearKeys[CurrentKey];
+        let AreaKeys = Object.keys(ProcessedData[YearKeys[CurrentKey]]);
+        let AreaAmount = AreaKeys.length;
+        //Summon part of graph
+        const GraphBody = document.getElementById("graphs")
+        const GraphArea = document.createElement("div");
+        GraphArea.className = "graph";
+        GraphArea.id = "Graph_" + CurrentKey;  
+        GraphArea.style.top = 500 * CurrentKey + "px";
+        const graph_area = document.createElement("div");
+        graph_area.className = "graph_area";
+        graph_area.id = "graph_area" + CurrentKey;
+        const graph_text = document.createElement("div");
+        graph_text.className = "graph_text"; 
+        GraphBody.appendChild(GraphArea);
+        GraphArea.appendChild(graph_area);
+        GraphArea.appendChild(graph_text);
+        
+
+
+
+        //get key in js object
+        for(let CurrentAreaKey = 0; CurrentAreaKey < AreaAmount; CurrentAreaKey ++){
+            //create graph components
+            const GraphSection = document.createElement("div");
+            GraphSection.class = "GraphSection";
+            GraphSection.id= "GraphSection_" + CurrentAreaKey;
+            graph_area.appendChild(GraphSection);
+
+
+
+            //get area in selected year
+            let Areas = Object.keys(ProcessedData[YearKeys[CurrentKey]]);
+            let CurrentFuelType = Object.keys(ProcessedData[YearKeys[CurrentKey]][Areas[CurrentAreaKey]]);
+            //Select vehicle fuel type
+            let AllFuelValues = [];
+            let SumOfAllValues = 0
+            for(let SelectedFuelType = 0; SelectedFuelType < CurrentFuelType.length; SelectedFuelType ++){
+                let CurrentSelectedFuelType = CurrentFuelType[SelectedFuelType];
+                //let CurrentFuelData = Object.keys(ProcessedData[YearKeys[CurrentKey]][Areas[CurrentAreaKey]][CurrentSelectedFuelType]);
+                let CurrentFuelData = ProcessedData[YearKeys[CurrentKey]][Areas[CurrentAreaKey]][CurrentSelectedFuelType];
+                AllFuelValues[SelectedFuelType] = parseInt(CurrentFuelData); 
+                SumOfAllValues = SumOfAllValues  + parseInt(CurrentFuelData)
+                
+            } 
+            //Calculate percentage
+            let AllFuelValuesPercentage = [];
+            for(let SelectedValue = 0; SelectedValue < AllFuelValues.length; SelectedValue ++){
+                let SelectedFuel = AllFuelValues[SelectedValue];
+                let PercentageValue = (SelectedFuel / SumOfAllValues) * 100; 
+                AllFuelValuesPercentage[SelectedValue] = PercentageValue; 
+            }
+
+
+
+            console.log(AllFuelValuesPercentage.sort());       
+             
+            
+
+            
+        }       
+
+
+
+
+
+    }
 }
--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Here's all the work i've done troughout the years. 
 Note that my code is usually spaghetti with absolutealy no documentation. Pls don't judge.
 BTW some images/files are removed due to copyright/file size constraints
+
+
+# Milestones 
+
+100th commit : 14.11.2023
+
