c482961b95d4bb38a88dca11a944af0398ba8eb0
~nz <81528500+ProtoSparky@users.noreply.github.com>
2023-11-20 15:52:11 +0100

    Update index.js

--- IT2/bits/20.11.2023_sector_diagram/assets/js/index.js
+++ IT2/bits/20.11.2023_sector_diagram/assets/js/index.js
@@ -2,7 +2,6 @@ var DataFilePath = "./data/";
 function init(){
     const DataFIle = DataFilePath + "1.csv";
     const CSVARR = ParseCSV(ReadAnything(DataFIle));
-
     console.log(CSVARR); 
     DrawSector(CSVARR);
 }
