bcd09fa9ab65756badcd47f22d7fcf5b7a34615a
~nz <81528500+ProtoSparky@users.noreply.github.com>
2023-12-04 15:51:17 +0100

    ai done

--- IT2/bits/4.12.2023_ai_prosAndCons/assets/js/main.js
+++ IT2/bits/4.12.2023_ai_prosAndCons/assets/js/main.js
@@ -1,3 +1,139 @@
+var DataLoc="./data/data.json";
+var JsonObject = ReadJSON(DataLoc,0);
+var JsonKeys = Object.keys(JsonObject);
+var ChapterKey = 0;
+
 function init(){
     console.info("init run");
+    GeneratePagesFromJson(JsonObject,"Hva er KI?","ContentArea");
+    SpawnNav();
+
+
+}
+
+function GeneratePagesFromJson(JsonObject,SelectedChapter,ParentID){
+    const CurrentChapterObject = JsonObject[SelectedChapter];
+    const CurrentChapterSentences = Object.keys(CurrentChapterObject);
+    const CurrentChapterSentencesAmount = CurrentChapterSentences.length;
+    let ParentElement = document.getElementById(ParentID);
+
+    for(let ChapterSentencePointer = 0; ChapterSentencePointer <CurrentChapterSentencesAmount;ChapterSentencePointer++ ){
+        const SelectedSentenceKey = CurrentChapterSentences[ChapterSentencePointer];
+        const SelectedChapter = CurrentChapterObject[SelectedSentenceKey];
+
+        if(SelectedChapter["img"] != undefined){
+            //If element is image
+            let CurrentElement = document.createElement("img");
+
+            if(SelectedChapter["class"] != undefined){
+                CurrentElement.className = SelectedChapter["class"]; 
+                //Adds class to element
+            }
+            if(SelectedChapter["src"] != undefined){
+                //Adds img source
+                CurrentElement.src = SelectedChapter["src"];
+            }
+            if(SelectedChapter["id"] != undefined){
+                CurrentElement.id = SelectedChapter["id"]; 
+                //Adds class to element
+            }
+            if(SelectedChapter["img"] != undefined){
+                CurrentElement.alt = SelectedChapter["img"]; 
+                //Adds class to element
+            }
+            ParentElement.appendChild(CurrentElement);
+        }
+        if(SelectedChapter["tx"] != undefined){
+            //If element is text
+            let CurrentElement = document.createElement("div");
+
+            if(SelectedChapter["class"] != undefined){
+                CurrentElement.className = SelectedChapter["class"]; 
+                //Adds class to element
+            }
+            if(SelectedChapter["src"] != undefined){
+                //Adds img source
+                const CurrentElementHyperlink = document.createElement("a");
+                CurrentElementHyperlink.href = SelectedChapter["src"];
+                if(SelectedChapter["tx"] != undefined){
+                    CurrentElementHyperlink.innerHTML = SelectedChapter["tx"]; 
+                    //Adds class to element
+                }
+                CurrentElement.appendChild(CurrentElementHyperlink);
+            }
+            else{
+                if(SelectedChapter["tx"] != undefined){
+                    CurrentElement.innerHTML = SelectedChapter["tx"]; 
+                    //Adds class to element
+                }
+            }
+            if(SelectedChapter["id"] != undefined){
+                CurrentElement.id = SelectedChapter["id"]; 
+                //Adds class to element
+            }
+            ParentElement.appendChild(CurrentElement);
+        }
+
+    }
+    
+}
+
+function SpawnNav(){
+    const NavArea = document.getElementById("topbar");
+    const NavRight = document.createElement("button");
+    NavRight.addEventListener("click",function(){
+        MoveChapters(true);
+    });
+    NavRight.className="button";
+    NavRight.id="RightNav";
+    NavRight.innerHTML = "Next";
+
+    const NavLeft = document.createElement("button");
+    NavLeft.addEventListener("click",function(){
+        MoveChapters(false);
+    });
+    NavLeft.innerHTML = "Back";
+    NavLeft.className="button";
+    NavLeft.id="LefttNav";
+    NavArea.appendChild(NavLeft); 
+    NavArea.appendChild(NavRight); 
+}
+function MoveChapters(Selector){
+    if(Selector){
+        if(ChapterKey < JsonKeys.length - 1){
+            ChapterKey += 1;
+            console.log(ChapterKey);
+            const CurrentChapterNames = JsonKeys[ChapterKey]; 
+            const PreviousContentArea = document.getElementById("ContentArea");
+            PreviousContentArea.remove();
+            const ContentArea = document.createElement("div");
+            ContentArea.id="ContentArea";
+            const ContentAreaParent = document.getElementById("content");
+            ContentAreaParent.appendChild(ContentArea);      
+            GeneratePagesFromJson(JsonObject,CurrentChapterNames,"ContentArea");
+            
+            //update header
+            PageHeader = document.getElementById("PageHeader");
+            PageHeader.innerHTML = CurrentChapterNames; 
+
+        }
+    }
+    else{
+        if(ChapterKey > 0){
+            ChapterKey -= 1;
+            console.log(ChapterKey);
+            const CurrentChapterNames = JsonKeys[ChapterKey]; 
+            const PreviousContentArea = document.getElementById("ContentArea");
+            PreviousContentArea.remove();
+            const ContentArea = document.createElement("div");
+            ContentArea.id="ContentArea";
+            const ContentAreaParent = document.getElementById("content");
+            ContentAreaParent.appendChild(ContentArea);      
+            GeneratePagesFromJson(JsonObject,CurrentChapterNames,"ContentArea");
+
+            //update header
+            PageHeader = document.getElementById("PageHeader");
+            PageHeader.innerHTML = CurrentChapterNames; 
+        }
+    }
 }
