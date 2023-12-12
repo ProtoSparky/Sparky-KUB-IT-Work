20c68f6a1dee3d60515ad97df5b9da5889199fda
~nz <81528500+ProtoSparky@users.noreply.github.com>
2023-12-12 09:51:14 +0100

    bunch of stuff added

--- IT2/bits/11.12.2023_Santa_shopping/assets/js/main.js
+++ IT2/bits/11.12.2023_Santa_shopping/assets/js/main.js
@@ -1,4 +1,6 @@
+
 var UsernamePrefix = "_usrNane";
+var CurrentUserName = null; 
 function init(){
     const SantaUsername = "santa"+ UsernamePrefix;
     const SantaAge = "1753"; 
@@ -15,12 +17,12 @@ function init(){
         }
     }
 
-    DataOP(3,true,"Accounts","null"); 
+    DataOP(3,true,"Accounts","null"); //Delete all site data
 
     if(DataOP(1,true,"Accounts","null") == undefined){
         console.info("Accounts empty, setting up new ones....")
         DataOP(0,true,"Accounts",SantaProfile); //set up santa's account
-        console.log(DataOP(1,true,"Accounts","null"));
+        //console.log(DataOP(1,true,"Accounts","null"));
         
     }
     else{
@@ -88,10 +90,7 @@ function init(){
         AccountWindowInputSubmit.innerHTML = "Create Account";
         AccountWindowInputSubmit.addEventListener("click",function(){
             CreateAccount();
-        });
-
-
-    
+        });    
 
 
 
@@ -104,14 +103,107 @@ function init(){
         AccountWindow.appendChild(AccountWindowInputSubmit);
 
     }
+    else{
+        //Spawn login menu
+        //TODO
+    }
 
 }
 function CreateAccount(){
     const AccountUserName = document.getElementById("AccountUserName").value;
     const AccountUserAge = document.getElementById("AccountUserAge").value;
-    GenerateMessageBanner(0,"test");
+    //GenerateMessageBanner(0,"Wrong password");
+
+    //create user profile 
+    const NewUserName = AccountUserName + UsernamePrefix;
+    let OldUserData = DataOP(1,true,"Accounts");
+    OldUserData[NewUserName] = {
+        "Age":AccountUserAge,
+        "IsSanta":false,
+        "wish":{
+
+        }
+    }
+    DataOP(0,true,"Accounts", OldUserData);
+    console.log(DataOP(1,true,"Accounts"));
+    document.getElementById("AccountWindow").remove(); 
+    CurrentUserName = AccountUserName;
+    
+    CheckWishList(CurrentUserName + UsernamePrefix);
 
 }
+
+
+function CheckWishList (username){
+    const SiteData = DataOP(1,true,"Accounts");
+
+    if(Object.keys(SiteData[username]["wish"]).length == 0){
+        //user has no wishes, spawn wish menu
+        console.log("User has no wishes"); 
+
+        const WishMenu = document.createElement("div");
+        WishMenu.style.position = "absolute";
+        WishMenu.style.transform  ="translate(-50%,-50%)";
+        WishMenu.style.top = "50%";
+        WishMenu.style.left = "50%";
+        WishMenu.style.width = "500px";
+        WishMenu.style.height = "500px";
+        WishMenu.style.backgroundColor = "var(--col_bg_lighter)";
+        WishMenu.style.borderRadius  = "var(--CornerRad)";
+        WishMenu.id = "WishMenu"; 
+
+        //Header
+        const WishMenuHeader = document.createElement("div");
+        WishMenuHeader.innerHTML = "Enter your wish";
+        WishMenuHeader.style.position = "absolute";
+        WishMenuHeader.style.top = "30px";
+        WishMenuHeader.className = "text";
+        WishMenuHeader.style.fontSize = "35";
+        WishMenuHeader.style.width = "100%";
+        WishMenuHeader.style.left = "0px";
+        WishMenuHeader.style.textAlign = "center";
+        WishMenuHeader.style.fontWeight = "500";
+        WishMenuHeader.style.color = "#f39c12";
+
+        //Seletor
+        const WishMenuWishType = document.createElement("select");
+        WishMenuWishType.style.position = "absolute";
+        WishMenuWishType.style.top = "250px";
+        WishMenuWishType.style.left = "50%";
+        WishMenuWishType.style.transform = "translate(-50%)";
+        WishMenuWishType.placeholder = "Wish type";
+        WishMenuWishType.id = "WishType";
+
+        const WishTypes = ["Abstract", "Material"]
+        for(let WishPointer = 0; WishPointer < 2; WishPointer ++){
+            const WishMenuWishTypeOption = document.createElement("option");
+            WishMenuWishTypeOption.value = WishTypes[WishPointer];
+            WishMenuWishTypeOption.innerHTML = WishTypes[WishPointer]; 
+            WishMenuWishType.appendChild(WishMenuWishTypeOption);
+            console.log(WishTypes[WishPointer]);
+        }
+
+
+        WishMenu.appendChild(WishMenuWishType); 
+        WishMenu.appendChild(WishMenuHeader);
+        document.body.appendChild(WishMenu);
+    }
+    else{
+        //spawn list of wishes and wish menu
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 function GenerateMessageBanner(FeedBackState, FeedBackText){
     //Feedbackstate 0 == Green, good
     //FeedBackState 1 == yellow, warning
@@ -119,16 +211,34 @@ function GenerateMessageBanner(FeedBackState, FeedBackText){
     const ColorOK = "#00d573";
     const ColorWarn = "#f39c12";
     const ColorErr = "#ff2225"; 
-    const BannerTime  = 500; // ms * 10
+    const ColorFall  ="#89a6a1"; 
+    const BannerTime  = 4; // s
     let CurrentBannerTime = 0; 
+    
 
     const MessageBanner = document.createElement("div");
     MessageBanner.style.position = "absolute";
     MessageBanner.style.zIndex = "99999999";
     MessageBanner.style.top = "30px";
+    MessageBanner.style.height = "30px";
+    MessageBanner.style.borderRadius = "var(--CornerRad)";
     MessageBanner.style.right = "-400px";
-    MessageBanner.style.width = "200px"; 
-    MessageBanner.id = "MessageBanner"; 
+    if(FeedBackText.length < 25){
+        MessageBanner.style.width = "200px";
+    }
+    else{
+        MessageBanner.style.width = FeedBackText.length * 10 + "px"; 
+    }
+    MessageBanner.id = "MessageBanner";
+    const MessageBannerText = document.createElement("div");
+    MessageBannerText.style.position = "absolute";
+    MessageBannerText.style.top = "50%";
+    MessageBannerText.style.transform = "translate(0,-50%)";
+    MessageBannerText.style.width = "100%";
+    MessageBannerText.style.color = "white";
+    MessageBannerText.style.fontFamily = "main_font";
+    MessageBannerText.style.fontWeight  ="400";
+    MessageBannerText.style.padding = "var(--ElementPadding)";
 
 
     if(FeedBackState == 0){
@@ -144,32 +254,30 @@ function GenerateMessageBanner(FeedBackState, FeedBackText){
         //Error message
         MessageBanner.style.backgroundColor = ColorErr; 
     }
-    MessageBanner.innerHTML = FeedBackText; 
+    else{
+        //fallback if
+        MessageBanner.style.backgroundColor = ColorFall;
+    }
+    MessageBannerText.innerHTML = FeedBackText; 
+    MessageBanner.appendChild(MessageBannerText);
     document.body.appendChild(MessageBanner);
 
-    const intervalId = setInterval(AnimateBanner, 10);
-    
+
+    MessageBanner.style.animationName = "BarMove";
+    MessageBanner.style.animationDuration = "5s";
+    MessageBanner.style.animationTimingFunction = "ease-in-out"
+
+
+    const intervalId = setInterval(AnimateBanner, 1000);
+    document.body.style.overflow = "hidden";
     function AnimateBanner(){
         CurrentBannerTime += 1;
-        if(CurrentBannerTime < BannerTime){
-            let BannerPost = document.getElementById("MessageBanner");
-            if(CurrentBannerTime < BannerTime / 5){
-                if(parseInt(BannerPost.style.right) < 0){
-                    BannerPost.style.right  = parseInt(BannerPost.style.right) + 10 + "px";
-                }
-            }
-            else if(CurrentBannerTime < (BannerTime / 5) * 5){
-                if(parseInt(BannerPost.style.right) > -400){
-                    BannerPost.style.right  = parseInt(BannerPost.style.right) - 10 + "px";
-                }
-                
-            }
-
-        }
-        else{
+        if(CurrentBannerTime > BannerTime){
             document.getElementById("MessageBanner").remove();
+            document.body.style.overflow = "visible";
             clearInterval(intervalId);
         }
+
     }
 
 }
