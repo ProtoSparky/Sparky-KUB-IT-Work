3d5cc103bc6cb3a544634a83a35e5c567d2fbd44
Sparky <kristupmaker@gmail.com>
2024-01-15 15:38:42 +0100

    2nd push

--- IT2/bits/15.01.2023_object_oriented/assets/js/Spark_eng.pCtrl.js
+++ IT2/bits/15.01.2023_object_oriented/assets/js/Spark_eng.pCtrl.js
@@ -2,6 +2,9 @@
 //-------------------------------------------------------------------------------------------------------------
 //------------------------------------Player CTRL--------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------
+
+//p_control is modified for this purpose
+
 const keys = {};
 document.addEventListener("keydown",(e) =>{
     keys[e.key] = true;
@@ -10,89 +13,17 @@ document.addEventListener("keyup",(e)=>{
     keys[e.key] = false;
 });
 function check_player_input(){
-    current_player_X = parseInt(player.style.left);
-    current_player_Y = parseInt(player.style.top);
     const key_W = keys["W"] || keys["w"];
     const key_A = keys["A"] || keys["a"];
     const key_S = keys["S"] || keys["s"];
-    const key_D = keys["D"] || keys["d"];
-
-    
-
-    if(key_W && key_A){
-        if(DEBUG){
-            console.log("wa"); 
-        }
-        //check if player is about to not collide, and move
-        if(!IScollided(current_player_X, player_sizeX,  current_player_Y - player_speed,player_sizeY, "col_mesh")[3] && !IScollided(current_player_X - player_speed, player_sizeX,  current_player_Y,player_sizeY, "col_mesh")[1]){
-            player.style.top = (current_player_Y - player_speed);
-            player.style.left = (current_player_X - player_speed);
-        }
-        else if(!IScollided(current_player_X, player_sizeX,  current_player_Y - player_speed,player_sizeY, "col_mesh")[3]){
-            player.style.top = (current_player_Y - player_speed);
-        }
-        else if(!IScollided(current_player_X - player_speed, player_sizeX,  current_player_Y,player_sizeY, "col_mesh")[1]){
-            player.style.left = (current_player_X - player_speed);
-        }
-    }
-    else if(key_W && key_D){
-        if(DEBUG){
-            console.log("wd");
-        }
-        //check if player is about to not collide, and move
-        if(!IScollided(current_player_X, player_sizeX,  current_player_Y - player_speed,player_sizeY, "col_mesh")[3] && !IScollided(current_player_X + player_speed, player_sizeX,  current_player_Y,player_sizeY, "col_mesh")[2]){
-            player.style.top = (current_player_Y - player_speed);
-            player.style.left = (current_player_X + player_speed);
-        }
-        else if(!IScollided(current_player_X, player_sizeX,  current_player_Y - player_speed,player_sizeY, "col_mesh")[3]){
-            player.style.top = (current_player_Y - player_speed);
-        }
-        else if(!IScollided(current_player_X + player_speed, player_sizeX,  current_player_Y,player_sizeY, "col_mesh")[2]){
-            player.style.left = (current_player_X + player_speed);
-        }
-    }
-    else if(key_A && key_S){
-        if(DEBUG){
-            console.log("as");
-        }
-        //check if player is about to not collide, and move
-        if(!IScollided(current_player_X - player_speed, player_sizeX,  current_player_Y,player_sizeY, "col_mesh")[1] && !IScollided(current_player_X, player_sizeX,  current_player_Y + player_speed,player_sizeY, "col_mesh")[4]){
-            player.style.left = (current_player_X - player_speed);
-            player.style.top = (current_player_Y + player_speed);
-        }
-        else if(!IScollided(current_player_X, player_sizeX,  current_player_Y + player_speed,player_sizeY, "col_mesh")[4]){
-            player.style.top = (current_player_Y + player_speed);   
-        }
-        else if(!IScollided(current_player_X - player_speed, player_sizeX,  current_player_Y,player_sizeY, "col_mesh")[1]){
-            player.style.left = (current_player_X - player_speed);
-        }    
-
-        
-        
-    }
-    else if(key_S && key_D){
-        if(DEBUG){
-            console.log("sd");
-        }
-        //check if player is about to not collide, and move
-        if(!IScollided(current_player_X + player_speed, player_sizeX,  current_player_Y,player_sizeY, "col_mesh")[2] && !IScollided(current_player_X, player_sizeX,  current_player_Y+ player_speed,player_sizeY, "col_mesh")[4]){
-            player.style.top = (current_player_Y + player_speed);
-            player.style.left = (current_player_X + player_speed);
-        }
-        else if (!IScollided(current_player_X, player_sizeX,  current_player_Y+ player_speed,player_sizeY, "col_mesh")[4]){
-            player.style.top = (current_player_Y + player_speed);
-        }
-        else if(!IScollided(current_player_X - player_speed, player_sizeX,  current_player_Y,player_sizeY, "col_mesh")[1] && !IScollided(current_player_X + player_speed, player_sizeX,  current_player_Y,player_sizeY, "col_mesh")[2]){
-            player.style.left = (current_player_X + player_speed);
-        }
-    }
-    else if(key_W){
+    const key_D = keys["D"] || keys["d"];    
+    if(key_W){
         if(DEBUG){
             console.log("w");
         }
         //check if player is about to not collide, and move
-        if(!IScollided(current_player_X, player_sizeX,  current_player_Y- player_speed,player_sizeY, "col_mesh")[3]){
-            player.style.top = (current_player_Y - player_speed);
+        if(!IScollided(GameState.player.position.x, GameState.player.size.x,  GameState.player.position.y - GameState.player.playerMaxSpeed,GameState.player.size.y, "col_mesh")[0]){
+            GameState.player.position.y=GameState.player.position.y - GameState.player.playerMaxSpeed;
         }
         
     }
@@ -101,8 +32,8 @@ function check_player_input(){
             console.log("a");
         }
         //check if player is about to not collide, and move
-        if(!IScollided(current_player_X - player_speed, player_sizeX,  current_player_Y,player_sizeY, "col_mesh")[1]){
-            player.style.left = (current_player_X - player_speed);
+        if(!IScollided(GameState.player.position.x - GameState.player.playerMaxSpeed, GameState.player.size.x,  GameState.player.position.y,GameState.player.size.y, "col_mesh")[2]){
+            GameState.player.position.x=GameState.player.position.x - GameState.player.playerMaxSpeed;
         }
     }
     else if(key_S){
@@ -110,8 +41,8 @@ function check_player_input(){
             console.log("s");
         }
         //check if player is about to not collide, and move
-        if(!IScollided(current_player_X, player_sizeX,  current_player_Y+player_speed,player_sizeY, "col_mesh")[4]){
-            player.style.top = (current_player_Y + player_speed);
+        if(!IScollided(GameState.player.position.x, GameState.player.size.x,  GameState.player.position.y - GameState.player.playerMaxSpeed,GameState.player.size.y, "col_mesh")[1]){
+            GameState.player.position.y= GameState.player.position.y + GameState.player.playerMaxSpeed;
         }
     }
     else if(key_D){
@@ -119,8 +50,8 @@ function check_player_input(){
             console.log("D");   
         }
         //check if player is about to not collide, and move
-        if(!IScollided(current_player_X + player_speed, player_sizeX,  current_player_Y,player_sizeY, "col_mesh")[2]){
-            player.style.left = (current_player_X + player_speed);
+        if(!IScollided(GameState.player.position.x + GameState.player.playerMaxSpeed, GameState.player.size.x,  GameState.player.position.y,GameState.player.size.y, "col_mesh")[3]){
+            GameState.player.position.x= GameState.player.position.x + GameState.player.playerMaxSpeed;
         }
     }
     
@@ -128,9 +59,11 @@ function check_player_input(){
     //Press E, and things in here will run.
     const debug_key = keys["E"] || keys["e"];
     if(debug_key){
-        
+        console.log(GameState.player);
     }
     /*DEBUG*/
+
+    
 
 }
 //-------------------------------------------------------------------------------------------------------------
--- IT2/bits/15.01.2023_object_oriented/assets/js/main.js
+++ IT2/bits/15.01.2023_object_oriented/assets/js/main.js
@@ -1,7 +1,136 @@
 var GameState = {
+    "player":{
+        "position":{
+            "x":0,
+            "y":0
+        },
+        "size":{
+            "x":40,
+            "y":40
+        },
+        "playerMaxSpeed":10,
+        "PlayerID":"player"
+        
+    },
+    "enemies":{
 
+    },
+    "hinderances":{
+
+    },
+    "player_area":{
+
+    }
 };
+var DEBUG = false;
 
 function init(){
-    console.info("init run");
+    console.info("INIT");
+    var player = document.createElement("div");
+    player.id = GameState.player.PlayerID;
+    player.className = "player";
+    player.style.position = "absolute";
+    player.style.top = GameState.player.position.y + "px";
+    player.style.left = GameState.player.position.x + "px";
+    player.style.width = GameState.player.size.x + "px";
+    player.style.height = GameState.player.size.y + "px";
+    player.style.borderColor = "white";
+    player.style.borderStyle = "dashed";
+    player.style.zIndex = 99;
+    document.getElementById("content").appendChild(player);
+    document.body.style.overflow = "hidden";
+
+
+    //spawn colision meshes
+
+    //top mesh
+    const col_mesh_top = document.createElement("div");
+    col_mesh_top.className = "col_mesh";
+    col_mesh_top.style.position = "absolute";
+    col_mesh_top.style.top = "-1px";
+    col_mesh_top.style.width = "1px";
+    col_mesh_top.style.left = "0px";
+    col_mesh_top.id = "col_mesh_top"; 
+    col_mesh_top.style.width = window.innerWidth + "px"; 
+    document.getElementById("content").appendChild(col_mesh_top);
+
+
+    //bottom mesh
+    const col_mesh_bottom = document.createElement("div");
+    col_mesh_bottom.className = "col_mesh";
+    col_mesh_bottom.style.position ="absolute";
+    col_mesh_bottom.id ="col_mesh_bottom";
+    col_mesh_bottom.style.width = window.innerWidth + "px";
+    col_mesh_bottom.style.bottom = "-1px";
+    col_mesh_bottom.style.height = "1px";
+    document.getElementById("content").appendChild(col_mesh_bottom);
+
+    //left mesh
+
+    const col_mesh_left = document.createElement("div");
+    col_mesh_left.style.position = "absolute";
+    col_mesh_left.style.width = "1px";
+    col_mesh_left.style.left = "-1px";
+    col_mesh_left.style.top = "0px";
+    col_mesh_left.style.height = window.innerHeight + "px";
+    col_mesh_left.className = "col_mesh";
+    col_mesh_left.id = "col_mesh_left";
+    document.getElementById("content").appendChild(col_mesh_left);
+
+    //right mesh
+    const col_mesh_right = document.createElement("div");
+    col_mesh_right.style.position = "absolute";
+    col_mesh_right.style.top = "0px";
+    col_mesh_right.style.right = "-1px";
+    col_mesh_right.className = "col_mesh";
+    col_mesh_right.id = "col_mesh_right";
+    col_mesh_right.style.height = window.innerHeight + "px";
+    col_mesh_right.style.width = "1px"; 
+    document.getElementById("content").appendChild(col_mesh_right);
+
+    //create safezones
+    const safezone1 = document.createElement("div");
+    safezone1.id = "safezone1";
+    safezone1.className = "safezone";
+    safezone1.style.position ="absolute";
+    safezone1.style.backgroundColor = "white";
+    safezone1.style.left = "0px";
+    safezone1.style.bottom = "0px";
+    safezone1.style.width = "200px";
+    safezone1.style.height = "80px";
+    safezone1.style.zIndex = 10;
+    document.getElementById("content").appendChild(safezone1); 
+
+    const safezone2 = document.createElement("div");
+    safezone2.id = "safezone2";
+    safezone2.className = "safezone";
+    safezone2.style.position ="absolute";
+    safezone2.style.backgroundColor = "white";
+    safezone2.style.right = "0px";
+    safezone2.style.bottom = "0px";
+    safezone2.style.width = "200px";
+    safezone2.style.height = "80px";
+    safezone2.style.zIndex = 10;
+    document.getElementById("content").appendChild(safezone2);
+
+    setInterval(ConstantUpdater,10);
+    console.log(document.getElementsByClassName("col_mesh"));
+}
+function ConstantUpdater(){
+    check_player_input();
+    ApplyPlayerState();
+
+
+}; 
+function ApplyPlayerState(){
+    document.getElementById(GameState.player.PlayerID).style.top = GameState.player.position.y;
+    document.getElementById(GameState.player.PlayerID).style.left = GameState.player.position.x;
+    for(let SafezonePointer = 0; SafezonePointer < document.getElementsByClassName("safezone").length -1; SafezonePointer ++){
+        if(IScollided(GameState.player.position.x , GameState.player.size.x,  GameState.player.position.y,GameState.player.size.y, "safezone")[SafezonePointer]){
+            document.getElementById(GameState.player.PlayerID).style.borderColor = "black";
+        }
+        else{
+            document.getElementById(GameState.player.PlayerID).style.borderColor = "white";
+        }
+    }
 }
