46bb05a5a6bfb69bb0d25aa0e05c5c624a38ae3a
Sparky <contact@protosparky.uk>
2024-02-27 10:38:52 +0100

    add setup

--- IT2/bits/26.02.2024_server_pinger/assets/js/main.js
+++ IT2/bits/26.02.2024_server_pinger/assets/js/main.js
@@ -1,28 +1,139 @@
-const API_LINK = window.location + "api/index.php";
+const clientSettings = {
+    "drop_shadow":{
+        "open":false
+    },
+    "API":{
+        "link":window.location + "api/index.php"
+    }
+}
 function init(){
-    Send2API();
+    SpawnMenus();
 
 }
+function SpawnMenus(){
 
+    const server_area = document.createElement("div");
+    server_area.style.position = "absolute";
+    server_area.style.top = "50%";
+    server_area.style.width = "1000px";
+    server_area.style.height = "600px";
+    server_area.style.left  ="50%";
+    server_area.style.transform = "translate(-50%,-50%)";
+    server_area.style.borderRadius = AccessCSSVar("--CornerRad")
+    server_area.style.backgroundColor = AccessCSSVar("--col_bg_lighter");
+    document.getElementById("content-fullscreen").appendChild(server_area);
 
-function Send2API(){
-    const query = {
+    //check for database, else load in pingers
+    const check_for_db = {
         "get":[
-            "is_data_present"
+          "is_data_present"
         ]
     };
 
-    fetch(API_LINK, {
+    fetch(clientSettings.API.link, {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(query)
+        body: JSON.stringify(check_for_db)
     })
     //.then(response=> console.log(response)); 
     .then(response => response.json())
-    .then(data => ParseAPI(data));
+    .then(data => Check4Db(data));
 }
-function ParseAPI(data){
-    console.log(data); 
+function Check4Db(data){
+    if(data.is_data_present == 1){
+        //proceed to showing pingers
+        console.info("database present");
+    }
+    else if(data.is_data_present == 0 ){
+        //display
+        console.info("database gone");
+        DropShadow();
+        const dropshadow = document.getElementById("dropshadow");
+        const textbox = document.createElement("div");
+        textbox.style.position = "absolute";
+        textbox.style.top = "50%";
+        textbox.style.width = "1000px";
+        textbox.style.height = "600px";
+        textbox.style.left  ="50%";
+        textbox.style.transform = "translate(-50%,-50%)";
+        dropshadow.appendChild(textbox);
+
+        //textbox text
+        const header = document.createElement("div");
+        header.className = "text";
+        header.style.color = AccessCSSVar("--col_highlight_TXT");
+        header.style.position = "absolute";
+        header.style.left = "50%";
+        header.style.top = "5%";
+        header.style.transform = "translate(-50%, 0)";
+        header.style.fontSize = "25";
+        header.style.fontWeight = "800";
+        header.innerHTML = "Let's set up the DB for ServerPinger";
+        textbox.appendChild(header);
+
+        //setup button
+        const setup_db_btn = document.createElement("button");
+        setup_db_btn.style.position = "absolute";
+        setup_db_btn.innerHTML = "Setup Data storage"; 
+        setup_db_btn.style.transform = "translate(-50%, 0)";
+        setup_db_btn.style.left = "50%";
+        setup_db_btn.style.top = "50%";
+        setup_db_btn.style.fontSize = "20";        
+        setup_db_btn.addEventListener("click",function(){
+            setupfirstserver();
+        });
+        textbox.appendChild(setup_db_btn);
+
+    }
+}
+function setupfirstserver(){
+    //write database data
+    const check_for_db = {
+        "post":[
+          "create_data_storage"
+        ]
+      };
+
+    fetch(clientSettings.API.link, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(check_for_db)
+    })
+    //.then(response=> console.log(response)); 
+    .then(response => response.json())
+    .then(data => spawn_setup2ndphase(data));
+}
+function spawn_setup2ndphase(data){
+    
+}
+
+
+function DropShadow(){
+    console.log("making dropshadow");
+    if(clientSettings.drop_shadow.open == false){
+        //create dropshadow
+        const dropshadow = document.createElement("div");
+        dropshadow.style.top = "0px";
+        dropshadow.style.left = "0px";
+        dropshadow.style.width = "100%";
+        dropshadow.style.height = "100%";
+        dropshadow.style.position = "absolute";
+        dropshadow.style.backgroundColor = AccessCSSVar("--col_bg_content");
+        dropshadow.style.opacity = "1";
+        dropshadow.id = "dropshadow";
+        dropshadow.style.zIndex = "9";
+        document.getElementById("content-fullscreen").appendChild(dropshadow);
+        clientSettings.drop_shadow.open  = true;
+    }
+    else if(clientSettings.drop_shadow.open  == true){
+        //remove dropshadow
+        clientSettings.drop_shadow.open  = false; 
+    }
+}
+function checkErrors(data){
+    console.log(data);
 }
