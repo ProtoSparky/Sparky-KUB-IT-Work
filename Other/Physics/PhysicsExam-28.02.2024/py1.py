c2e41a25a4373ecd4d3911f2dad11cf94e25ec20
Sparky <contact@protosparky.uk>
2024-02-28 16:02:04 +0100

    added physics

---
+++ Other/Physics/PhysicsExam-28.02.2024/py1.py
@@ -0,0 +1,14 @@
+acc = 8.7
+size = 20 #cm 
+area = (size*size) * 10^-2
+weightcar = 0.25 #kg
+weightweight = 2 #kg
+magnet_strength = 0.3 #T
+resistance = 0.040 #ohm
+car2fieldgap = 20*10^-2 #m
+
+current_speed = 0
+
+while True:
+    current_speed = current_speed * acc
+    
