75b59118b1c4301985f8b8dda7cd097bd364ad48
Sparky <contact@protosparky.uk>
2024-03-04 10:23:10 +0100

    physics yay

--- Other/Physics/PhysicsExam-28.02.2024/py1.py
+++ Other/Physics/PhysicsExam-28.02.2024/py1.py
@@ -1,14 +1,33 @@
+import math as math
 acc = 8.7
-size = 20 #cm 
-area = (size*size) * 10^-2
+size_car = 20 * 10^-2 #cm 
+area = (size_car*size_car)
 weightcar = 0.25 #kg
 weightweight = 2 #kg
 magnet_strength = 0.3 #T
 resistance = 0.040 #ohm
 car2fieldgap = 20*10^-2 #m
+max_length = 0.2 #max max length of runner
+sim_stepping = 0.001
+start_velocity = math.sqrt(2*acc* max_length)
 
-current_speed = 0
+total_distance = 0
+VelocityLast = 0
+CurrentTime = 0
+CurrentAcc = acc
+CurrentDistance = 0
 
-while True:
-    current_speed = current_speed * acc
+
+while total_distance < max_length:
+    CurrentTime = CurrentTime + sim_stepping
+    VelocityLast = CurrentAcc * CurrentTime
+    total_distance = total_distance + VelocityLast + CurrentTime
+
+    current_ems = math.sqrt(2*CurrentAcc *CurrentDistance) * magnet_strength * size_car
+    current_brake_force = ((current_ems)/(resistance)) * size_car *magnet_strength
+
+    print(current_brake_force)
+
+
+    
     
