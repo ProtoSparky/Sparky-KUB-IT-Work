5ae445b9ff2cc2c3a9bdb1a506c6e29f0f469c08
Sparky <contact@protosparky.uk>
2024-03-11 14:21:51 +0100

    added catalogue for js functions

--- IT2/bits/11.03.2024_python/1.py
+++ IT2/bits/11.03.2024_python/1.py
@@ -1,1 +1,11 @@
-def kalorier():    hastighet = input("Skriv inn hastighet: ");    hastighet = float(hastighet.replace(",","."));        if hastighet >60 or hastighet <=0:        print("Din hastighet er ikke sannsynlig")        input("Press enter for ny start")        kalorier() if __name__ == '__main__':    kalorier()
+def kalorier():    
+    hastighet = input("Skriv inn hastighet: ");   
+    hastighet = float(hastighet.replace(",","."));        
+    if hastighet >60 or hastighet <=0:        
+        print("Din hastighet er ikke sannsynlig")        
+        input("Press enter for ny start")    
+        kalorier()     
+        
+
+if __name__ == '__main__':
+        kalorier()
