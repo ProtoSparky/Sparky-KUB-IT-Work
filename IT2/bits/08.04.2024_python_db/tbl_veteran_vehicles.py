64c2a3811ff869ac31756994978cb3c6f4455a9a
Sparky <contact@protosparky.uk>
2024-04-09 08:56:23 +0200

    work going

--- IT2/bits/08.04.2024_python_db/tbl_veteran_vehicles.py
+++ IT2/bits/08.04.2024_python_db/tbl_veteran_vehicles.py
@@ -5,4 +5,15 @@ today = datetime.date.today()
 year = today.year
 veteran_year = year - 30
 connect = testdb.connect('uvUke33.db')
-print(veteran_year)
+
+#produsent, modell, drivstoff, gearbox, km_stand, prod_year, regnr, skilttype, farge, sitteplasser
+
+#get a list of all veteran vehicles
+vehicles = []
+with connect:
+    sql = "SELECT * FROM tbl_bruktbiler WHERE prod_year <" + str(veteran_year)
+    data = connect.execute(sql)
+    for row in data: 
+        vehicles.append(row)
+
+#sort vehicles alphabetically
