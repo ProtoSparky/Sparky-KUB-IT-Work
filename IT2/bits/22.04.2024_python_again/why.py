0bb920d2028b13ca86d973be4d7bfcfece784de6
Sparky <contact@protosparky.uk>
2024-04-23 10:16:30 +0200

    Oh no

--- IT2/bits/22.04.2024_python_again/why.py
+++ IT2/bits/22.04.2024_python_again/why.py
@@ -1,6 +1,8 @@
 import pandas
 import matplotlib.pyplot as plt
-from datetime import datetime
+import datetime
+import numpy as np
+import matplotlib.dates as mdates # Correct import statement
 #import dependency file
 file_path = "./GlobalLandTemperaturesByCity.csv"
 file_array = pandas.read_csv(file_path,delimiter=",")
@@ -16,7 +18,7 @@ while pointer < total_keys:
     #iterate trough each row
     if(file_array["City"][pointer] == find_city):
         print("found city " + file_array["City"][pointer] + " " + str(pointer))
-        city_index.append([file_array["dt"][pointer],file_array["City"][pointer],float(file_array["AverageTemperature"][pointer])])
+        city_index.append([file_array["dt"][pointer],float(file_array["AverageTemperature"][pointer])])
         city_found = True
     elif(city_found == True and file_array["City"][pointer] != find_city):
         #stops loop once all keys are found
@@ -27,8 +29,24 @@ print("------")
 print(city_index)
 
 
-#plot all data
-plt.style.use('ggplot') 
-plt.figure('Degrees for ' + find_city)
-plt.plot(city_index["dt"], city_index["AverageTemperature"])
+
+
+
+dates = [datetime.datetime.strptime(date, "%Y-%m-%d") for date, _ in city_index]
+values = [value for _, value in city_index]
+
+# Plotting
+fig, ax = plt.subplots()
+ax.plot(dates, values)
+
+# Adjusting the locator to generate ticks for each year
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=365 * 50)) # every 10 years
+#ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
+ax.xaxis.set_major_locator(mdates.YearLocator())
+
+plt.title('Temperatures for ' + find_city + " 1700 - 2013")
+plt.xlabel('Date')
+plt.ylabel('Temperature (C)')
+plt.style.use('ggplot')
+plt.gcf().autofmt_xdate() # Rotate date labels automatically
 plt.show()
