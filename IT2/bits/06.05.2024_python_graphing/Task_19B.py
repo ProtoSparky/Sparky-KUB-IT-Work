8844eef1233800bf2724447d588d0aa6994d677e
Sparky <contact@protosparky.uk>
2024-05-06 14:16:34 +0200

    removed brainfuck

--- IT2/bits/06.05.2024_python_graphing/Task_19B.py
+++ IT2/bits/06.05.2024_python_graphing/Task_19B.py
@@ -1,10 +1,23 @@
 import matplotlib.pyplot as plt
-biking_distances = [10, 40, 50, 14, 50, 50, 40]
+biking_distances = [20, 30, 0, 14, 45, 30, 60]
 biking_days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+temp_array = []
 
-plt.plot(biking_days, biking_distances)
+biking_distance_len = len(biking_distances)
+pointer = 0
+while pointer < biking_distance_len:
+    if(pointer < 1):
+        temp_array.append(biking_distances[pointer])
+    else:
+        print(biking_distances[pointer -1])
+        temp_array.append(int(temp_array[pointer -1]) + int(biking_distances[pointer]))
+    pointer +=1 
+
+print(temp_array)
+plt.plot(biking_days, temp_array)
 plt.title("Biking distances (KM) for the weekdays")
 plt.xlabel('Date')
 plt.ylabel('Distance in KM')
 plt.style.use('ggplot')
 plt.show()
+
--- IT2/bits/06.05.2024_python_graphing/Task_19C.py
+++ IT2/bits/06.05.2024_python_graphing/Task_19C.py
@@ -1,4 +1,19 @@
 import matplotlib.pyplot as plt
+import json
+biking_distances = [20, 30, 0, 14, 45, 30, 60]
+biking_days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+multi_user_distances = {
+    "Ole":[20, 30, 0, 14, 45, 30, 60],
+    "Jens":[10, 40, 50, 14, 50, 50, 40]
+}
+multi_user_increasing = {}
+
+
+
+
+
+
 
 plt.plot(biking_days, biking_distances)
 plt.title("Biking distances (KM) for the weekdays")
