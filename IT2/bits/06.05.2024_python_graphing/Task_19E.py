66322b0e85e63df218d95ab06d42ee5e98070418
Sparky <contact@protosparky.uk>
2024-05-06 15:45:10 +0200

    fun

--- IT2/bits/06.05.2024_python_graphing/Task_19E.py
+++ IT2/bits/06.05.2024_python_graphing/Task_19E.py
@@ -3,14 +3,15 @@ import matplotlib.pyplot as plt
 UserData = {}
 Temp = None
 def InputUsername():
-    '''
+    
     username = input("Enter username: ")
     UserData[username] = {}
     UserData[username]["days"] = InputDays()
     UserData[username]["distance"] = InputDistance(username)
+    UserData[username]["temp"] = InputTemperature(username)
+    Sum_total_km()
     print(UserData)
-    '''
-    print(Ask4Number("monday"))
+    
 
 def InputDays():
     Inputting = True
@@ -33,19 +34,47 @@ def InputDistance(username):
     user_days = selected_user["days"]
     temp_array = []
     for select_day in user_days: 
-        km = Ask4Number(select_day)
-        temp_array.append(km)
+        km = Ask4Number(select_day, "Enter distance biked for ")
+        while km[1]:
+            km = Ask4Number(select_day, "Enter distance biked for ")
+        temp_array.append(km[0])
     return temp_array
-        
 
-def Ask4Number(current_day):
-    distance = input("Enter distance biked for " + current_day + " ")
+def InputTemperature(username): 
+    selected_user = UserData[username]
+    user_days = selected_user["days"]
+    temp_array = []
+    for select_day in user_days: 
+        km = Ask4Number(select_day, "Enter Temperature for ")
+        while km[1]:
+            km = Ask4Number(select_day, "Enter Temperature for ")
+        temp_array.append(km[0])
+    return temp_array 
+
+def Ask4Number(current_day, InputText):
+    distance = input(InputText + current_day + " ")
     try:
         val = int(distance)
-        return val
+        return [val, False]
     except ValueError:
         print("That's not a number!")
-        distance = Ask4Number(current_day)
+        return [None, True]
+
+def Sum_total_km():
+    all_users = UserData.keys()
+    for select_user in all_users:
+        select_user_data = UserData[select_user]
+        distance = select_user_data["distance"]
+        distance_len = len(distance)
+        dist_pointer = 0 
+        UserData[select_user]["distance_sum"] = []
+        while dist_pointer < distance_len:
+            if(dist_pointer < 1):                
+                UserData[select_user]["distance_sum"].append(distance[dist_pointer])
+            else:
+                UserData[select_user]["distance_sum"].append(int(UserData[select_user]["distance_sum"][dist_pointer -1]) + int(distance[dist_pointer]))
+            dist_pointer +=1
+
 
 
 #start the script
