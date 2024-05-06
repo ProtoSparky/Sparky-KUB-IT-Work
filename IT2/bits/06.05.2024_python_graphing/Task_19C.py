a30560a1dfc127795ccacd58b4e4368f128859d0
Sparky <contact@protosparky.uk>
2024-05-06 14:48:59 +0200

    this is annoying

--- IT2/bits/06.05.2024_python_graphing/Task_19C.py
+++ IT2/bits/06.05.2024_python_graphing/Task_19C.py
@@ -1,6 +1,4 @@
 import matplotlib.pyplot as plt
-import json
-biking_distances = [20, 30, 0, 14, 45, 30, 60]
 biking_days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 multi_user_distances = {
@@ -9,15 +7,34 @@ multi_user_distances = {
 }
 multi_user_increasing = {}
 
+users = multi_user_distances.keys()
+
+
+#iterate trough users and append to temp array
+for current_user in users:
+    selected_user_distance = multi_user_distances[current_user]
+    biking_length = len(selected_user_distance)
+    multi_user_increasing[current_user] = []
+
+    pointer = 0
+    while pointer < biking_length:
+        if(pointer < 1):
+            multi_user_increasing[current_user].append(selected_user_distance[pointer])
+        else:
+            multi_user_increasing[current_user].append(int(multi_user_increasing[current_user][pointer -1]) + int(selected_user_distance[pointer]))
+        pointer +=1 
+
+    ##add plot for selected user
+    plt.plot(biking_days, multi_user_increasing[current_user], label  = current_user)
 
 
 
 
-
-
-plt.plot(biking_days, biking_distances)
-plt.title("Biking distances (KM) for the weekdays")
+#plt.plot(biking_days, biking_distances)
+plt.title("Biking distances (KM) for a week")
 plt.xlabel('Date')
 plt.ylabel('Distance in KM')
 plt.style.use('ggplot')
+plt.legend() 
 plt.show()
+plt.savefig('Biking distances_(KM)_for_a_week.png') #save as image
---
+++ IT2/bits/06.05.2024_python_graphing/Task_19D.py
@@ -0,0 +1,57 @@
+#this script inputs distance driven, and plots that
+import matplotlib.pyplot as plt
+biking_days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+temp_array = {}
+temp2_array = {}
+def Inputs():
+    username = input("Enter username: ")
+    temp_array[username] = [] #init array
+
+    for current_day in biking_days: 
+        value = Ask4Number(current_day)
+        temp_array[username].append(value)
+    temp2_array = sum_totals(username,temp_array)
+    plotter(temp2_array)
+
+
+    
+    
+
+def Ask4Number(current_day):
+    distance = input("Enter distance biked for " + current_day + " ")
+    try:
+        val = int(distance)
+        return val
+    except ValueError:
+        print("That's not a number!")
+        Ask4Number(current_day)
+
+
+def sum_totals(username, array):
+    selected_user_data = array[username]
+    user_inputs = len(selected_user_data)
+    pointer = 0
+    tmp_array = {}
+    tmp_array[username] = []
+    while pointer < user_inputs:
+        if(pointer < 1):
+            tmp_array[username].append(selected_user_data[pointer])
+        else:
+            tmp_array[username].append(int(tmp_array[username][pointer -1]) + int(selected_user_data[pointer]))
+        pointer +=1 
+    return(tmp_array)
+
+
+def plotter(temp2_array):
+    for selected_user in temp2_array:
+        plt.plot(biking_days, temp2_array[selected_user], label  = selected_user)
+    
+    plt.title("Biking distances (KM) for a week")
+    plt.xlabel('Date')
+    plt.ylabel('Distance in KM')
+    plt.style.use('ggplot')
+    plt.legend() 
+    plt.show()
+
+
+Inputs()
