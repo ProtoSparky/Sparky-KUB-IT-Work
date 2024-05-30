f9a5692cc721953673e2d23f65516d00e0832f30
Sparky <81528500+ProtoSparky@users.noreply.github.com>
2024-05-30 13:46:45 +0200

    finished prep for exam

--- IT2/bits/30.05.2024_exam_prepare/playstore_viewer.py
+++ IT2/bits/30.05.2024_exam_prepare/playstore_viewer.py
@@ -1,4 +1,5 @@
 import tools as tk
+import numpy
 #Lag et program som presenterer en oversikt over de tre største kategoriene målt i antall apper.
 #Oversikten skal vise antallet apper, gjennomsnittsratingen og det gjennomsnittlige antallet 
 #installasjoner for hver av disse tre kategoriene.
@@ -33,5 +34,49 @@ for current_cat in categories:
 length_by_category_sorted = tk.sort_dict(length_by_category, ascending = False)
 
 
+def GetAppRatingsAndInstallations(decending_app_amount, sorted_appList, category_keys,csv_file ):
+    category_list = list(sorted_appList.keys()) 
+    first_x = []
+    pointer = 0
+    #We get the first x amount of categories for use later
+    while pointer < decending_app_amount:
+        first_x.append(category_list[pointer])
+        pointer += 1
+
+    return_data = {}
+    #we iterate trough this category
+    for current_category in first_x: 
+        #get all array keys for given category 
+        current_category_keys = category_keys[current_category] 
+
+        temp_rating = []
+        temp_installs = []
+        for current_category_key in current_category_keys: 
+            #we iterate trough all array keys from the csv file for the given category
+            current_cating = csv_file["Rating"][current_category_key]
+            if(current_cating != "NaN"): #some of the values contain NaN. We want to filter away those as tht will skew our results
+                current_cating = float(current_cating)
+                temp_rating.append(current_cating)
+
+            #now we check for the installs
+            current_installs = csv_file["Installs"][current_category_key]
+            if(current_installs != "NaN"): 
+                current_installs = current_installs.replace("+","")
+                current_installs = current_installs.replace(",","")
+                current_installs = int(current_installs)
+                temp_installs.append(current_installs)
+        temp_rating_mean = numpy.mean(temp_rating) #We make a mean or average number from the array
+        temp_installs_mean = numpy.mean(temp_installs)
+        return_data[current_category] = { #We construct a containier for which we return all data
+            "average_installs":temp_installs_mean, 
+            "average_ratings":temp_rating_mean, 
+            "app_amount": sorted_appList[current_category]
+        }
+    return return_data #We return the data
 
 
+
+
+
+
+print(GetAppRatingsAndInstallations(3, length_by_category_sorted, categories,csv_file))
